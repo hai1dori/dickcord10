79ff09bd2a38c234721d7d61e1a4358d7bd27c08
hai1dori <haiala050609@student.jenseneducation.se>
2023-05-03 13:56:53 +0200

    Add project files.

---
+++ dickcord10/Net/Server.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//kommer använda sockets
+using System.Net.Sockets;
+
+namespace dickcord10.Net
+{
+    internal class Server
+    {
+
+        TcpClient _client;
+            //construktooorr 
+       public Server()
+        {
+            _client = new TcpClient();
+         }
+
+        public void ConnectToServer()
+        {
+            if (!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
